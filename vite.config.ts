8165fb46a7f7405e8bad441c0448b7f384ce6e0b
Yujifu <yujf@tianyuaninfo.com>
2025-09-22 17:11:23 +0800

    修改前端跨域配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+        // 不重写路径，保持 /api 前缀
+        // rewrite: (path) => path.replace(/^\/api/, '')  // 注释掉这行
+      }
+    }
+  }
 })
